60741918e612bbb39b99af86868689ce82b47353
sondn3008 <sondn3008@gmail.com>
2022-07-14 21:24:08 +0700

    fix small

--- src/api/routers/user.router.js
+++ src/api/routers/user.router.js
@@ -2,13 +2,22 @@ import express from 'express';
 import validate from '../validation/user.validation';
 import userService from '../services/user.service';
 import authMdw from '../middlewares/auth.mdw';
+import fileSaveHelper from '../helpers/file/FileSaveHelper';
+import multer from 'multer';
+const upload = multer();
+
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
+router.post('/register', upload.single('image'), async (req, res) => {
   const data = req.body;
+  const file = req.file;
   const checkData = validate.registerValidate(data);
   if (checkData.error != null) {
     return res.status(400).json({ message: checkData.error.details[0].message });
+  }
+  if (file) {
+    const saveImage = await fileSaveHelper.saveImage(file);
+    data.image = saveImage.url;
   }
   const result = await userService.registerUser(data);
   return res.status(result.statusCode).json(result.json);
