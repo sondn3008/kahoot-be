e96cea84202ade0836643ac7aa73a06bc060775c
sondn3008 <sondn3008@gmail.com>
2022-07-21 19:36:05 +0700

    fix expire jwt

--- src/api/middlewares/auth.mdw.js
+++ src/api/middlewares/auth.mdw.js
@@ -1,19 +1,27 @@
 import jwt from 'jsonwebtoken';
+import { isJwtExpired } from 'jwt-check-expiration';
 import 'dotenv/config';
 
 // còn trường hợp expire
 export default function (req, res, next) {
-  const accessToken = req.headers['kahootApp-access-token'];
+  const accessToken = req.headers['kahootapp-access-token'];
+  console.log(isJwtExpired(accessToken));
   if (accessToken) {
-    try {
-      const decoded = jwt.verify(accessToken, process.env.SECRET_KEY);
-      // console.log(decoded);
-      req.accessTokenPayload = decoded;
-      next();
-    } catch (err) {
-      console.error(err);
+    if (isJwtExpired(accessToken) === false) {
+      try {
+        const decoded = jwt.verify(accessToken, process.env.SECRET_KEY);
+        // console.log(decoded);
+        req.accessTokenPayload = decoded;
+        next();
+      } catch (err) {
+        console.error(err);
+        return res.status(401).json({
+          message: 'Invalid accessToken',
+        });
+      }
+    } else {
       return res.status(401).json({
-        message: 'Invalid accessToken',
+        message: 'AccessToken has expired',
       });
     }
   } else {
